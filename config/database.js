394f853ba23041b337c9011db3a5475097cf57bb
Brandie <89561618+BrandieChanel@users.noreply.github.com>
2021-10-25 10:04:33 -0400

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : "mongodb+srv://Btech:mongo123@cluster0.6gudl.mongodb.net/roulette?retryWrites=true&w=majority", 
+    'dbName': 'roulette'
+};
